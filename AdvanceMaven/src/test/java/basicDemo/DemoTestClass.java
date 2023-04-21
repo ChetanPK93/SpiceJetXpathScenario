6dbcc397d717935d3968cf5099c5eb901174db3a
Gaming <Gaming@Asus>
2023-04-21 13:12:07 +0530

    To learn about git hub commit and push extra class is added in default package

--- AdvanceMaven/src/test/java/basicDemo/DemoTestClass.java
+++ AdvanceMaven/src/test/java/basicDemo/DemoTestClass.java
@@ -4,6 +4,6 @@ public class DemoTestClass
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Git Hub class push testing");
+		System.out.println("Git Hub for class push testing");
 	}
 }
